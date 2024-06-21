398fa060dcd77991fed46170b883fbb42ffe89cc
Sasa <sasa@ikigailabs.io>
2024-06-21 20:11:17 +0200

    Space component

---
+++ src/components/Space/Space.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import {
+  Space as AntSpace,
+  SpaceProps as AntSpaceProps,
+  ConfigProvider,
+} from "antd";
+import lightTheme from "../../styles/lightAntdTheme";
+import styles from "./Space.module.css";
+import classNames from "classnames/bind";
+let cx = classNames.bind(styles);
+
+const SpaceStyles: any = {};
+
+export const Space = (props: AntSpaceProps) => {
+  const classes: any = props?.className
+    ? `${styles.iki_space} ${props?.className}`
+    : styles.iki_space;
+  return (
+    <ConfigProvider
+      wave={{ disabled: true }}
+      theme={{
+        token: {
+          //motion: false,
+          fontFamily: "Inter, sans-serif",
+          ...lightTheme,
+        },
+        components: { Space: SpaceStyles },
+      }}
+    >
+      <AntSpace {...props} className={classes}>
+        {props?.children}
+      </AntSpace>
+    </ConfigProvider>
+  );
+};
